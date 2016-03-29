6ef2a8ad6aa06c0d6f57989fa760294173518bb3
Barbora Ulipova <b.ulipova@gmail.com>
2016-03-29 16:25:42 +0200

    isbn and title can be added

--- add_book_script.php
+++ add_book_script.php
@@ -1,21 +1,15 @@
 <?php
-echo $_POST['nazev'];
-
 $link = mysqli_connect("localhost:3306", "root","","simplelibrary");
 
 if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
     exit;
 }
 
 else {
-	$new_isbn = (string)$_POST['isbn'];
-	var_dump($new_isbn);
-	$sql_query = "INSERT INTO `publications`(`isbn`) VALUES (".$new_isbn.")";
+	
+	$sql_query = "INSERT INTO `publications`(`isbn`,`title`) VALUES ('".$_POST['isbn']."','".$_POST['nazev']."')";
 	mysqli_query($link, $sql_query);
 }
 
 mysqli_close($link);
-?>
